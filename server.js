003b51f128c72a5f3702c5b07b28727b4fb06eb3
wafaankoush99 <wafadirawe@gmail.com>
2021-04-02 15:57:45 +0300

    portfolio"

---
+++ server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// make variable to use express library:
+const expressLibrary = require('express');
+
+// to has all express properties and methods:
+const server = expressLibrary();
+const PORT = process.env.PORT || 3000;
+
+// to access a file in the public directory
+server.use(expressLibrary.static('./public'));
+
+// route definitions
+// localHost:3000/
+server.get('/',(request,response)=>{
+    response.send('route')
+});
+
+// localHost:3000/test 
+server.get('/test',(request,response)=>{
+    response.send('Server Is Alive')
+})
+
+// localHost:3000/data 
+server.get('/data',(request,response)=>{
+    let names = [
+        { userName: 'Wafa'},
+        { userName: 'Dania'},
+        { userName: 'Tasnim'},
+        { userName: 'Heba'}
+    ]
+    response.json(names)
+})
+
+// server listening
+server.listen(PORT,()=>{
+    console.log(`Listening on PORT ${PORT}`);
+})
+
